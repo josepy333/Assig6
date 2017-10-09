163adb7931fb29b2b5d45ae4529b36726320be0f
mokawad <31768193+mokawad@users.noreply.github.com>
2017-10-08 21:38:00 -0500

    Update Assig6.java

--- Assig6.java
+++ Assig6.java
@@ -2,9 +2,30 @@
 public class Assig6
 {
 
+   static int MAX_CARDS_PER_HAND = 56;
+   static int NUM_PLAYERS = 2;
+   
    public static void main(String[] args)
    {
-      // TODO Auto-generated method stub
+      int numPacksPerDeck = 1;
+      int numJokersPerPack = 0;
+      int numUnusedCardsPerPack = 0;
+      int numOfPlayers = 2;
+      int cardsPerHand = 7;
+      Card[] unusedCardsPerPack = null;
+      
+      CardGameFramework highCardGame = new CardGameFramework( 
+            numPacksPerDeck, numJokersPerPack,  
+            numUnusedCardsPerPack, unusedCardsPerPack, 
+            numOfPlayers, cardsPerHand);
+      
+      ClockTimer timer = new ClockTimer();
+      timer.startTimer();
+      
+      GameModel model = new GameModel(highCardGame, "Computer", "Player");
+      GameView view = new GameView(timer);
+      
+      GameControl game = new GameControl(model, view);
 
    }
 
